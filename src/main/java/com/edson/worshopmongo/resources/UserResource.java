037c9e1dee8c42a933e53158ca3ed4ae46951e89
Edson Gomes <edson.bol.santos@hotmail.com>
2023-08-27 15:44:02 -0300

    EndPoint para retornar os posts do usuario

--- src/main/java/com/edson/worshopmongo/resources/UserResource.java
+++ src/main/java/com/edson/worshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.edson.worshopmongo.domain.Post;
 import com.edson.worshopmongo.domain.User;
 import com.edson.worshopmongo.dto.UserDTO;
 import com.edson.worshopmongo.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build() ;
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
