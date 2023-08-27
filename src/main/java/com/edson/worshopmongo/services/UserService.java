116963620c0ad1ace375c8b6f22cc7472f6f1707
Edson Gomes <edson.bol.santos@hotmail.com>
2023-08-27 14:29:41 -0300

    insercao de usuario com post

--- src/main/java/com/edson/worshopmongo/services/UserService.java
+++ src/main/java/com/edson/worshopmongo/services/UserService.java
@@ -1,21 +1,36 @@
 package com.edson.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edson.worshopmongo.domain.User;
+import com.edson.worshopmongo.dto.UserDTO;
 import com.edson.worshopmongo.repository.UserRepository;
+import com.edson.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
